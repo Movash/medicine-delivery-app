11f83f66341053cfa4b22f7fd00e263ae9620a60
Movash <andrushhenko97@gmail.com>
2024-03-01 22:56:44 +0200

    add layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import Header from 'components/Header/Header';
+import Loader from '../Loader/Loader';
+import React, { Suspense } from 'react';
+import { Outlet } from 'react-router';
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </>
+  );
+};
+
+export default Layout;
