ab1de784166285880ee3f8dd68eecd2c6f7aa322
Movash <andrushhenko97@gmail.com>
2024-03-04 00:03:24 +0200

    add main logic

--- src/pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -2,36 +2,102 @@ import {
   ShoppingCartCont,
   LeftBlock,
   RightBlock,
+  SelectedCardContainer,
+  SelectedCardImage,
+  CardTitleCont,
+  CountCont,
+  ButtonsCont,
+  CounterButton,
+  CountNumber,
+  XButton,
+  Price,
 } from './ShoppingCartPage.styled';
-import {
-  CardContainer,
-  CardImage,
-  CardTitle,
-  AddButton,
-  CardList,
-  ShopHeader,
-} from '../ShopsPage/ShopsPage.styled';
+import { CardTitle, CardList, ShopHeader } from '../ShopsPage/ShopsPage.styled';
 import placeholder from '../../images/stock-illustration-drugs-and-pills.jpg';
 import UserForm from 'components/UserForm/UserForm';
+import { useEffect, useState } from 'react';
+import { getStorageData, setStorageData } from 'helpers/storage';
+import sprite from '../../images/sprite.svg';
+
+const baseURL = 'https://nodejs-medicine-delivery.onrender.com';
 
 const ShoppingCartPage = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const storedCartItems = getStorageData('cartItems') || [];
+    setCartItems(storedCartItems);
+  }, []);
+
+    const handleAddOne = index => {
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[index].count++;
+      setCartItems(updatedCartItems);
+      setStorageData('cartItems', updatedCartItems);
+    };
+
+  const handleSubtractOne = index => {
+    const updatedCartItems = [...cartItems];
+    updatedCartItems[index].count = Math.max(
+      updatedCartItems[index].count - 1,
+      1
+    );
+    setCartItems(updatedCartItems);
+    setStorageData('cartItems', updatedCartItems);
+  };
+
+  const handleRemoveFromCart = index => {
+    const updatedCartItems = [...cartItems];
+    updatedCartItems.splice(index, 1);
+    setCartItems(updatedCartItems);
+    setStorageData('cartItems', updatedCartItems);
+  };
+
   return (
     <ShoppingCartCont>
       <LeftBlock>
         <UserForm />
       </LeftBlock>
       <RightBlock>
-        <ShopHeader>Товари</ShopHeader>
+        <ShopHeader>Selected drugs</ShopHeader>
         <CardList>
-          <CardContainer>
-            <CardImage src={placeholder} alt='img' />
-            <CardTitle>name</CardTitle>
-            <AddButton>Додати</AddButton>
-          </CardContainer>
+          {cartItems.map((medicine, index) => (
+            <SelectedCardContainer key={medicine._id}>
+              <XButton onClick={() => handleRemoveFromCart(index)}>
+                <svg width="32" height="32" viewBox="0 0 16 16">
+                  <use href={`${sprite}#icon-x`} />
+                </svg>
+              </XButton>
+              <SelectedCardImage
+                src={
+                  medicine.photo ? `${baseURL}/${medicine.photo}` : placeholder
+                }
+                alt={medicine.name}
+              />
+              <CardTitleCont>
+                <CardTitle>
+                  {medicine.name.charAt(0).toUpperCase() +
+                    medicine.name.slice(1)}
+                </CardTitle>
+                <Price>Price: {medicine.price * (medicine.count || 1)}$</Price>
+                <CountCont>
+                  <CountNumber>{medicine.count}</CountNumber>
+                  <ButtonsCont>
+                    <CounterButton onClick={() => handleAddOne(index)}>
+                      ⬆
+                    </CounterButton>
+                    <CounterButton onClick={() => handleSubtractOne(index)}>
+                      ⬇
+                    </CounterButton>
+                  </ButtonsCont>
+                </CountCont>
+              </CardTitleCont>
+            </SelectedCardContainer>
+          ))}
         </CardList>
       </RightBlock>
     </ShoppingCartCont>
   );
 };
 
-export default ShoppingCartPage
+export default ShoppingCartPage;
--- src/pages/ShoppingCartPage/ShoppingCartPage.styled.js
+++ src/pages/ShoppingCartPage/ShoppingCartPage.styled.js
@@ -13,3 +13,78 @@ export const LeftBlock = styled.div`
 export const RightBlock = styled.div`
   width: 50%;
 `;
+
+export const SelectedCardContainer = styled.div`
+  position: relative;
+  width: 750px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  border-radius: 12px;
+  overflow: hidden;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const SelectedCardImage = styled.img`
+  width: 60%;
+  height: 310px;
+  margin-left: 20px;
+  object-fit: cover;
+  border-radius: 12px;
+`;
+
+export const CardTitleCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 40%;
+  height: 100%;
+`;
+
+export const Price = styled.p`
+  font-weight: 400;
+  font-size: 20px;
+  letter-spacing: 1.5;
+  margin-bottom: 20px;
+`;
+
+export const CountCont = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100px;
+  height: 50px;
+  border-radius: 15px;
+  background-color: var(--gray-color);
+  border: 1px solid var(--black-color);
+`;
+
+export const CountNumber = styled.p`
+  margin-left: 25px;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 1.5;
+`;
+
+export const ButtonsCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 15px;
+  color: var(--gray-color);
+`;
+
+export const CounterButton = styled.button`
+  border-radius: 10px;
+  background-color: var(--gray-color);
+`;
+
+export const XButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: var(--white-color);
+  margin-top: 10px;
+  margin-right: 8px;
+`;
