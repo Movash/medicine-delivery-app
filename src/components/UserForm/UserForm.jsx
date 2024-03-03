63463576ecea4adf8a5d28b1fd86dc89029b560f
Movash <andrushhenko97@gmail.com>
2024-03-04 00:04:43 +0200

    change form

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -3,274 +3,202 @@ import { formSchema } from '../../schemas/FormSchema/FormSchema';
 import { FormCont } from './UserForm.styled';
 import sprite from '../../images/sprite.svg';
 
-const UserForm = ({ registration }) => {
+const UserForm = () => {
+  const onSubmit = ({ name, email, phone, address }, actions) => {
+    console.log(name);
+    console.log(email);
+    console.log(phone);
+    console.log(address);
+  };
 
-  // const onSubmit = async ({ name, email, password }, actions) => {
-  //   registration({
-  //     name: name,
-  //     email: email,
-  //     password: password,
-  //   });
-  //   await new Promise(resolve => setTimeout(resolve, 1000));
-  // };
-
-  // const formik = useFormik({
-  //   initialValues: {
-  //     name: '',
-  //     email: '',
-  //     password: '',
-  //   },
-  //   validationSchema: formSchema,
-  //   onSubmit,
-  // });
-
-    const onSubmit = async ({ name, email, phone, address }, actions) => {
-      registration({
-        name: name,
-        email: email,
-        phone: phone,
-        address: address,
-      });
-      await new Promise(resolve => setTimeout(resolve, 1000));
-    };
-
-    const formik = useFormik({
-      initialValues: {
-        name: '',
-        email: '',
-        phone: '',
-        address: '',
-      },
-      validationSchema: formSchema,
-      onSubmit,
-    });
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+    },
+    validationSchema: formSchema,
+    onSubmit,
+  });
 
   return (
     <FormCont>
-      <div>
-        <form onSubmit={formik.handleSubmit}>
-          <div className="inputs-cont">
-            <div>
-              <input
-                autoComplete="off"
-                name="name"
-                type="text"
-                placeholder="Name"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                className={
-                  formik.errors.name &&
-                  formik.touched.name &&
-                  formik.values.name.length > 0
-                    ? 'input-error'
-                    : formik.touched.name && formik.values.name.length > 0
-                    ? 'input-success'
-                    : ''
-                }
-              />
-
-              {!formik.errors.name &&
+      <form onSubmit={formik.handleSubmit}>
+        <div className="inputs-cont">
+          <div className="input-cont">
+            <input
+              autoComplete="off"
+              name="name"
+              type="text"
+              placeholder="Name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={
+                formik.errors.name &&
                 formik.touched.name &&
-                formik.values.name.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-checkbox-circle-fill`} />
-                    </svg>
-                    <p className="notify-descr success">Success name</p>
-                  </div>
-                )}
-              {formik.errors.name &&
-                formik.touched.name &&
-                formik.values.name.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-red`} />
-                    </svg>
-                    <p className="notify-descr error">{formik.errors.name}</p>
-                  </div>
-                )}
-            </div>
-            <div>
-              <input
-                autoComplete="off"
-                name="email"
-                type="email"
-                placeholder="Email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                className={
-                  formik.errors.email &&
-                  formik.touched.email &&
-                  formik.values.email.length > 0
-                    ? 'input-error'
-                    : formik.touched.email && formik.values.email.length > 0
-                    ? 'input-success'
-                    : ''
-                }
-              />
+                formik.values.name.length > 0
+                  ? 'input-error'
+                  : formik.touched.name && formik.values.name.length > 0
+                  ? 'input-success'
+                  : ''
+              }
+            />
 
-              {!formik.errors.email &&
-                formik.touched.email &&
-                formik.values.email.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-checkbox-circle-fill`} />
-                    </svg>
-                    <p className="notify-descr success">Success email</p>
-                  </div>
-                )}
-              {formik.errors.email &&
-                formik.touched.email &&
-                formik.values.email.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-red`} />
-                    </svg>
-                    <p className="notify-descr error">{formik.errors.email}</p>
-                  </div>
-                )}
-            </div>
-            {/* <div>
-              <input
-                autoComplete="off"
-                name="password"
-                type="text"
-                placeholder="Password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                className={
-                  formik.errors.password &&
-                  formik.touched.password &&
-                  formik.values.password.length > 0
-                    ? 'input-error'
-                    : formik.touched.password &&
-                      formik.values.password.length > 0
-                    ? 'input-success'
-                    : ''
-                }
-              />
-
-              {!formik.errors.password &&
-                formik.touched.password &&
-                formik.values.password.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-checkbox-circle-fill`} />
-                    </svg>
-                    <p className="notify-descr success">Success password</p>
-                  </div>
-                )}
-              {formik.errors.password &&
-                formik.touched.password &&
-                formik.values.password.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-red`} />
-                    </svg>
-                    <p className="notify-descr error">
-                      {formik.errors.password}
-                    </p>
-                  </div>
-                )}
-            </div> */}
-            <div>
-              <input
-                autoComplete="off"
-                name="phone"
-                type="tel"
-                placeholder="Phone"
-                value={formik.values.phone}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                className={
-                  formik.errors.phone &&
-                  formik.touched.phone &&
-                  formik.values.phone.length > 0
-                    ? 'input-error'
-                    : formik.touched.phone && formik.values.phone.length > 0
-                    ? 'input-success'
-                    : ''
-                }
-              />
-
-              {!formik.errors.phone &&
-                formik.touched.phone &&
-                formik.values.phone.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-checkbox-circle-fill`} />
-                    </svg>
-                    <p className="notify-descr success">Success phone</p>
-                  </div>
-                )}
-              {formik.errors.phone &&
-                formik.touched.phone &&
-                formik.values.phone.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-red`} />
-                    </svg>
-                    <p className="notify-descr error">{formik.errors.phone}</p>
-                  </div>
-                )}
-            </div>
-            <div>
-              <input
-                autoComplete="off"
-                name="address"
-                type="text"
-                placeholder="Address"
-                value={formik.values.address}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                className={
-                  formik.errors.address &&
-                  formik.touched.address &&
-                  formik.values.address.length > 0
-                    ? 'input-error'
-                    : formik.touched.address &&
-                      formik.values.address.length > 0
-                    ? 'input-success'
-                    : ''
-                }
-              />
-
-              {!formik.errors.address &&
-                formik.touched.address &&
-                formik.values.address.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-checkbox-circle-fill`} />
-                    </svg>
-                    <p className="notify-descr success">Success address</p>
-                  </div>
-                )}
-              {formik.errors.address &&
-                formik.touched.address &&
-                formik.values.address.length > 0 && (
-                  <div className="notify-cont">
-                    <svg width="16" height="16" viewBox="0 0 16 16">
-                      <use href={`${sprite}#icon-red`} />
-                    </svg>
-                    <p className="notify-descr error">
-                      {formik.errors.address}
-                    </p>
-                  </div>
-                )}
-            </div>
+            {!formik.errors.name &&
+              formik.touched.name &&
+              formik.values.name.length > 0 && (
+                <div className="notify-cont">
+                  <svg width="16" height="16" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-checkbox-circle-fill`} />
+                  </svg>
+                  <p className="notify-descr success">Success name</p>
+                </div>
+              )}
+            {formik.errors.name &&
+              formik.touched.name &&
+              formik.values.name.length > 0 && (
+                <div className="notify-cont">
+                  <svg width="16" height="16" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+                  <p className="notify-descr error">{formik.errors.name}</p>
+                </div>
+              )}
           </div>
-          <button
-            type="submit"
-            className="submit-btn"
-            disabled={formik.isSubmitting}
-          >
-            Sign Up
-          </button>
-        </form>
-      </div>
+          <div className="input-cont">
+            <input
+              autoComplete="off"
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={
+                formik.errors.email &&
+                formik.touched.email &&
+                formik.values.email.length > 0
+                  ? 'input-error'
+                  : formik.touched.email && formik.values.email.length > 0
+                  ? 'input-success'
+                  : ''
+              }
+            />
+
+            {!formik.errors.email &&
+              formik.touched.email &&
+              formik.values.email.length > 0 && (
+                <div className="notify-cont">
+                  <svg width="16" height="16" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-checkbox-circle-fill`} />
+                  </svg>
+                  <p className="notify-descr success">Success email</p>
+                </div>
+              )}
+            {formik.errors.email &&
+              formik.touched.email &&
+              formik.values.email.length > 0 && (
+                <div className="notify-cont">
+                  <svg width="16" height="16" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+                  <p className="notify-descr error">{formik.errors.email}</p>
+                </div>
+              )}
+          </div>
+          <div className="input-cont">
+            <input
+              autoComplete="off"
+              name="phone"
+              type="tel"
+              placeholder="Phone"
+              value={formik.values.phone}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={
+                formik.errors.phone &&
+                formik.touched.phone &&
+                formik.values.phone.length > 0
+                  ? 'input-error'
+                  : formik.touched.phone && formik.values.phone.length > 0
+                  ? 'input-success'
+                  : ''
+              }
+            />
+
+            {!formik.errors.phone &&
+              formik.touched.phone &&
+              formik.values.phone.length > 0 && (
+                <div className="notify-cont">
+                  <svg width="16" height="16" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-checkbox-circle-fill`} />
+                  </svg>
+                  <p className="notify-descr success">Success phone</p>
+                </div>
+              )}
+            {formik.errors.phone &&
+              formik.touched.phone &&
+              formik.values.phone.length > 0 && (
+                <div className="notify-cont">
+                  <svg width="16" height="16" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+                  <p className="notify-descr error">{formik.errors.phone}</p>
+                </div>
+              )}
+          </div>
+          <div className="input-cont">
+            <input
+              autoComplete="off"
+              name="address"
+              type="text"
+              placeholder="Address"
+              value={formik.values.address}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              className={
+                formik.errors.address &&
+                formik.touched.address &&
+                formik.values.address.length > 0
+                  ? 'input-error'
+                  : formik.touched.address && formik.values.address.length > 0
+                  ? 'input-success'
+                  : ''
+              }
+            />
+
+            {!formik.errors.address &&
+              formik.touched.address &&
+              formik.values.address.length > 0 && (
+                <div className="notify-cont">
+                  <svg width="16" height="16" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-checkbox-circle-fill`} />
+                  </svg>
+                  <p className="notify-descr success">Success address</p>
+                </div>
+              )}
+            {formik.errors.address &&
+              formik.touched.address &&
+              formik.values.address.length > 0 && (
+                <div className="notify-cont">
+                  <svg width="16" height="16" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-red`} />
+                  </svg>
+                  <p className="notify-descr error">{formik.errors.address}</p>
+                </div>
+              )}
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="submit-btn"
+          disabled={formik.isSubmitting}
+        >
+          Submit
+        </button>
+      </form>
     </FormCont>
   );
 };
--- src/components/UserForm/UserForm.styled.js
+++ src/components/UserForm/UserForm.styled.js
@@ -2,7 +2,13 @@ import { styled } from 'styled-components';
 
 export const FormCont = styled('div')(() => {
   return {
-    marginTop: '66px',
+    marginTop: '100px',
+    form: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
     input: {
       fontSize: '14px',
       lineHeight: 1.29,
@@ -10,8 +16,7 @@ export const FormCont = styled('div')(() => {
       height: '46px',
       border: '1px solid rgba(239, 237, 232, 0.3)',
       borderRadius: '12px',
-      backgroundColor: 'orange',
-      // color: 'var(--white-color)',
+      backgroundColor: 'var(--light-active-color)',
       textIndent: '12px',
       outline: 'none',
       paddingTop: '2px',
@@ -23,8 +28,13 @@ export const FormCont = styled('div')(() => {
     '.inputs-cont': {
       display: 'flex',
       flexDirection: 'column',
+      alignItems: 'center',
       gap: '18px',
       marginBottom: '28px',
+      width: '100%',
+    },
+    '.input-cont': {
+      width: '60%',
     },
     '.input-error': {
       borderColor: 'var(--error-color)',
@@ -52,74 +62,17 @@ export const FormCont = styled('div')(() => {
       fontSize: '16px',
       lineHeight: 1.12,
       padding: '12px 40px',
-      // color: 'var(--white-color)',
       borderRadius: '12px',
-
       marginBottom: '12px',
-      backgroundColor: 'var(--orange-color)',
+      backgroundColor: 'var(--light-active-color)',
       transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1)',
       '&:hover, &:focus': {
-        backgroundColor: 'var(--light-orange-color)',
+        backgroundColor: 'var(--active-color)',
       },
-      '&:disabled': {
-        color: 'rgba(239, 237, 232, 0.6)',
-        cursor: 'not-allowed',
-      },
-    },
-    '.navigate-cont': {
-      display: 'flex',
-      fontSize: '12px',
-    },
-    '.navigate-descr': {
-      opacity: 0.6,
-    },
-    '.navigate-link': {
-      textDecoration: 'underline',
-      transition: 'color 250ms cubic-bezier(0.4, 0, 0.2, 1)',
-      '&:hover, &:focus': {
-        color: 'var(--light-orange-color)',
-      },
-    },
-    '@media screen and (min-width: 768px)': {
-      marginTop: '105px',
-      h1: {
-        fontSize: '32px',
-        lineHeight: 1.375,
-        marginBottom: '16px',
-      },
-      '.main-descr': {
-        width: '496px',
-        fontSize: '16px',
-        lineHeight: 1.5,
-        marginBottom: '32px',
-      },
-      input: {
-        fontSize: '16px',
-        lineHeight: 1.5,
-        width: '364px',
-        height: '52px',
-      },
-      '.inputs-cont': {
-        gap: '20px',
-        marginBottom: '64px',
-      },
-      '.eye-cont': {
-        width: 'fit-content',
-      },
-      '.eye-btn': {
-        top: '15px',
-      },
-      '.submit': {
-        fontSize: '20px',
-        lineHeight: 1.2,
-        padding: '16px 60px',
-      },
-      '.navigate-cont': {
-        marginLeft: '5px',
-      },
-    },
-    '@media screen and (min-width: 1440px)': {
-      marginTop: '116px',
+      // '&:disabled': {
+      //   color: 'rgba(239, 237, 232, 0.6)',
+      //   cursor: 'not-allowed',
+      // },
     },
   };
 });
