5a87da42c63e3beb4cfea97b76bf917e873bf378
Movash <andrushhenko97@gmail.com>
2024-03-05 19:44:00 +0200

    add_new_block_and_lots_of_styles

--- src/pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -11,8 +11,13 @@ import {
   CountNumber,
   XButton,
   Price,
+  AdditionalBlock,
+  ContentCartCont,
+  SelectedCardList,
+  SubmitButton,
+  SelectedTotal,
 } from './ShoppingCartPage.styled';
-import { CardTitle, CardList, ShopHeader } from '../ShopsPage/ShopsPage.styled';
+import { CardTitle, ShopHeader } from '../ShopsPage/ShopsPage.styled';
 import placeholder from '../../images/stock-illustration-drugs-and-pills.jpg';
 import UserForm from 'components/UserForm/UserForm';
 import { useEffect, useState } from 'react';
@@ -29,12 +34,12 @@ const ShoppingCartPage = () => {
     setCartItems(storedCartItems);
   }, []);
 
-    const handleAddOne = index => {
-      const updatedCartItems = [...cartItems];
-      updatedCartItems[index].count++;
-      setCartItems(updatedCartItems);
-      setStorageData('cartItems', updatedCartItems);
-    };
+  const handleAddOne = index => {
+    const updatedCartItems = [...cartItems];
+    updatedCartItems[index].count++;
+    setCartItems(updatedCartItems);
+    setStorageData('cartItems', updatedCartItems);
+  };
 
   const handleSubtractOne = index => {
     const updatedCartItems = [...cartItems];
@@ -55,47 +60,57 @@ const ShoppingCartPage = () => {
 
   return (
     <ShoppingCartCont>
-      <LeftBlock>
-        <UserForm />
-      </LeftBlock>
-      <RightBlock>
-        <ShopHeader>Selected drugs</ShopHeader>
-        <CardList>
-          {cartItems.map((medicine, index) => (
-            <SelectedCardContainer key={medicine._id}>
-              <XButton onClick={() => handleRemoveFromCart(index)}>
-                <svg width="32" height="32" viewBox="0 0 16 16">
-                  <use href={`${sprite}#icon-x`} />
-                </svg>
-              </XButton>
-              <SelectedCardImage
-                src={
-                  medicine.photo ? `${baseURL}/${medicine.photo}` : placeholder
-                }
-                alt={medicine.name}
-              />
-              <CardTitleCont>
-                <CardTitle>
-                  {medicine.name.charAt(0).toUpperCase() +
-                    medicine.name.slice(1)}
-                </CardTitle>
-                <Price>Price: {medicine.price * (medicine.count || 1)}$</Price>
-                <CountCont>
-                  <CountNumber>{medicine.count}</CountNumber>
-                  <ButtonsCont>
-                    <CounterButton onClick={() => handleAddOne(index)}>
-                      ⬆
-                    </CounterButton>
-                    <CounterButton onClick={() => handleSubtractOne(index)}>
-                      ⬇
-                    </CounterButton>
-                  </ButtonsCont>
-                </CountCont>
-              </CardTitleCont>
-            </SelectedCardContainer>
-          ))}
-        </CardList>
-      </RightBlock>
+      <ContentCartCont>
+        <LeftBlock>
+          <UserForm />
+        </LeftBlock>
+        <RightBlock>
+          <ShopHeader>Selected drugs</ShopHeader>
+          <SelectedCardList>
+            {cartItems.map((medicine, index) => (
+              <SelectedCardContainer key={medicine._id}>
+                <XButton onClick={() => handleRemoveFromCart(index)}>
+                  <svg width="32" height="32" viewBox="0 0 16 16">
+                    <use href={`${sprite}#icon-x`} />
+                  </svg>
+                </XButton>
+                <SelectedCardImage
+                  src={
+                    medicine.photo
+                      ? `${baseURL}/${medicine.photo}`
+                      : placeholder
+                  }
+                  alt={medicine.name}
+                />
+                <CardTitleCont>
+                  <CardTitle>
+                    {medicine.name.charAt(0).toUpperCase() +
+                      medicine.name.slice(1)}
+                  </CardTitle>
+                  <Price>
+                    Price: {medicine.price * (medicine.count || 1)}$
+                  </Price>
+                  <CountCont>
+                    <CountNumber>{medicine.count}</CountNumber>
+                    <ButtonsCont>
+                      <CounterButton onClick={() => handleAddOne(index)}>
+                        ⬆
+                      </CounterButton>
+                      <CounterButton onClick={() => handleSubtractOne(index)}>
+                        ⬇
+                      </CounterButton>
+                    </ButtonsCont>
+                  </CountCont>
+                </CardTitleCont>
+              </SelectedCardContainer>
+            ))}
+          </SelectedCardList>
+        </RightBlock>
+      </ContentCartCont>
+      <AdditionalBlock>
+        <SelectedTotal>Total price: 999</SelectedTotal>
+        <SubmitButton type="button">Submit</SubmitButton>
+      </AdditionalBlock>
     </ShoppingCartCont>
   );
 };
--- src/pages/ShoppingCartPage/ShoppingCartPage.styled.js
+++ src/pages/ShoppingCartPage/ShoppingCartPage.styled.js
@@ -1,17 +1,26 @@
 import styled from 'styled-components';
 
 export const ShoppingCartCont = styled.div`
-  margin-top: 100px;
+  margin-top: 60px;
+`;
+export const ContentCartCont = styled.div`
   display: flex;
+  height: calc(85vh - 60px);
+  border-bottom: 3px solid var(--sky-blue-color);
 `;
 
 export const LeftBlock = styled.div`
   width: 50%;
-  border-right: 1px solid var(--blue-color);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 export const RightBlock = styled.div`
+  height: 100%;
   width: 50%;
+  background-color: var(--gray-color);
+  border-left: 3px solid var(--sky-blue-color);
 `;
 
 export const SelectedCardContainer = styled.div`
@@ -23,6 +32,8 @@ export const SelectedCardContainer = styled.div`
   border-radius: 12px;
   overflow: hidden;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  background-color: var(--white-color);
+  margin: 10px;
 `;
 
 export const SelectedCardImage = styled.img`
@@ -45,7 +56,7 @@ export const CardTitleCont = styled.div`
 export const Price = styled.p`
   font-weight: 400;
   font-size: 20px;
-  letter-spacing: 1.5;
+  line-height: 1.5;
   margin-bottom: 20px;
 `;
 
@@ -87,4 +98,42 @@ export const XButton = styled.button`
   background-color: var(--white-color);
   margin-top: 10px;
   margin-right: 8px;
+`;
+
+export const AdditionalBlock = styled.div`
+  height: 15vh;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+`;
+
+export const SelectedCardList = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  overflow-y: auto;
+  height: calc(100% - 87.6px);
+`;
+
+export const SelectedTotal = styled.p`
+  margin-right: 200px;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.43;
+`;
+
+export const SubmitButton = styled.button`
+  margin-right: 50px;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 1.43;
+  color: var(--white-color);
+  border-radius: 16px;
+  padding: 12px 100px;
+  background-color: var(--sky-blue-color);
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover,
+  &:focus {
+    background-color: var(--blue-color);
+  }
 `;
