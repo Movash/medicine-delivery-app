15a03eb3fdd8a9f4ce14d690f62a8e14a7a9a2e0
Movash <andrushhenko97@gmail.com>
2024-03-09 21:31:05 +0200

    add_toaster

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { App } from 'App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from 'styles/GlobalStiles/GlobalStyles';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/">
       <GlobalStyle />
       <App />
+      <Toaster />
     </BrowserRouter>
   </React.StrictMode>
 );
