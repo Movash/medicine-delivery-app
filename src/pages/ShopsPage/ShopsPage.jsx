9e90b730b0c61bf369524a0fcc0625ee5c412466
Movash <andrushhenko97@gmail.com>
2024-03-11 22:31:20 +0200

    transfer_some_logic

--- src/pages/ShopsPage/ShopsPage.jsx
+++ src/pages/ShopsPage/ShopsPage.jsx
@@ -5,20 +5,17 @@ import {
   RightBlock,
   ShopList,
   ShopItem,
-  CardContainer,
-  CardImage,
-  CardTitle,
-  AddButton,
-  CardList,
   ShopHeader,
+  // CardContainer,
+  // CardImage,
+  // CardTitle,
+  // AddButton,
+  // CardList,
+  // SvgHeart,
 } from './ShopsPage.styled';
-import placeholder from '../../images/stock-illustration-drugs-and-pills.jpg';
 import { getAll } from 'api/Pharmacy.api';
 import Loader from 'components/Loader/Loader';
-import { getStorageData, setStorageData } from 'helpers/storage';
-import toast from 'react-hot-toast';
-
-const baseURL = 'https://nodejs-medicine-delivery.onrender.com';
+import ShopsList from 'components/ShopsList/ShopsList';
 
 const ShopsPage = () => {
   const [shops, setShops] = useState([]);
@@ -47,28 +44,6 @@ const ShopsPage = () => {
     setSelectedShop(shop);
   };
 
-  const handleAddToCart = medicine => {
-    const cartItems = getStorageData('cartItems') || [];
-
-    const isItemAlreadyAdded = cartItems.some(
-      item => item.name === medicine.name
-    );
-
-    if (isItemAlreadyAdded) {
-      toast.error('Already added', {
-        duration: 3000,
-        position: 'top-center',
-      });
-    } else {
-      toast.success('Successfully added to shopping cart', {
-        duration: 3000,
-        position: 'top-center',
-      });
-      const updatedCartItems = [...cartItems, medicine];
-      setStorageData('cartItems', updatedCartItems);
-    }
-  };
-
   return (
     <ShopsCont>
       {error && <h1>{error}</h1>}
@@ -86,27 +61,7 @@ const ShopsPage = () => {
       {selectedShop && (
         <RightBlock>
           <ShopHeader>Medicines in {selectedShop.pharmacy}</ShopHeader>
-          <CardList>
-            {selectedShop.medicines.map(medicine => (
-              <CardContainer key={medicine._id}>
-                <CardImage
-                  src={
-                    medicine.photo
-                      ? `${baseURL}/${medicine.photo}`
-                      : placeholder
-                  }
-                  alt={medicine.name}
-                />
-                <CardTitle>
-                  {medicine.name.charAt(0).toUpperCase() +
-                    medicine.name.slice(1)}
-                </CardTitle>
-                <AddButton onClick={() => handleAddToCart(medicine)}>
-                  Add to Cart
-                </AddButton>
-              </CardContainer>
-            ))}
-          </CardList>
+          <ShopsList selectedShop={selectedShop} />
         </RightBlock>
       )}
     </ShopsCont>
