c4dab85bbd8619c7c73376d226600ea4ae418678
Movash <andrushhenko97@gmail.com>
2024-03-12 22:30:21 +0200

    change_import

--- src/components/ShopsList/ShopsList.jsx
+++ src/components/ShopsList/ShopsList.jsx
@@ -1,12 +1,10 @@
 import ShopsListItem from 'components/ShopsListItem/ShopsListItem';
 import { getStorageData } from 'helpers/storage';
-import { CardList } from 'pages/ShopsPage/ShopsPage.styled';
+import { CardList } from './ShopsList.styled';
 
 const ShopsList = props => {
   const { selectedShop } = props;
-  // const favoriteMedicines = new Set(
-  //   (getStorageData('favoriteMedicines') || []).map(med => med._id)
-  // );
+
   const favoriteMedicines = new Set(
     getStorageData('favoriteMedicines')?.map(med => med.name) || []
   );
