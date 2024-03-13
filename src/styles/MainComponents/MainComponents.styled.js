2995ece6f37fa88dd575ab7ed7038029165f3720
Movash <andrushhenko97@gmail.com>
2024-03-13 20:41:54 +0200

    add_main_styles

---
+++ src/styles/MainComponents/MainComponents.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ShopHeader = styled.h2`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+  margin-top: 30px;
+`;
+
+export const CardTitle = styled.h3`
+  font-size: 18px;
+  margin: 15px 0;
+  font-weight: 600;
+  font-size: 28px;
+`;
