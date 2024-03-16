ae94eefe8ae200444fd01d02851da88565fd2f0f
Movash <andrushhenko97@gmail.com>
2024-03-16 20:18:01 +0200

    change_some_values

--- src/styles/MainComponents/MainComponents.styled.js
+++ src/styles/MainComponents/MainComponents.styled.js
@@ -1,13 +1,13 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ShopHeader = styled.h2`
   display: flex;
   justify-content: center;
-  margin-bottom: 30px;
-  margin-top: 30px;
+  margin-bottom: 20px;
+  margin-top: 20px;
 `;
 
-export const CardTitle = styled.h3`
+export const CartTitle = styled.h3`
   font-size: 18px;
   margin: 15px 0;
   font-weight: 600;
