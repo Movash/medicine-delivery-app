a012721d2b3f2cafc2d50149fab9f8fedd835c71
Movash <andrushhenko97@gmail.com>
2024-03-16 20:22:52 +0200

    create_cart_list_styles

---
+++ src/components/ShoppingCartList/ShoppingCartList.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SelectedCartList = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  overflow-y: auto;
+  height: calc(100% - 87.6px);
+`;
