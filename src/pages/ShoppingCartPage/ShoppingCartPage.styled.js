4ac081c7c2310d2e8a1e2f35a6b8401427af8cc1
Movash <andrushhenko97@gmail.com>
2024-03-16 20:19:14 +0200

    relocate_values

--- src/pages/ShoppingCartPage/ShoppingCartPage.styled.js
+++ src/pages/ShoppingCartPage/ShoppingCartPage.styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ShoppingCartCont = styled.div`
   margin-top: 60px;
 `;
+
 export const ContentCartCont = styled.div`
   display: flex;
   height: calc(85vh - 60px);
@@ -23,96 +24,11 @@ export const RightBlock = styled.div`
   border-left: 3px solid var(--sky-blue-color);
 `;
 
-export const SelectedCardContainer = styled.div`
-  position: relative;
-  width: 750px;
-  height: 350px;
-  display: flex;
-  align-items: center;
-  border-radius: 12px;
-  overflow: hidden;
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  background-color: var(--white-color);
-  margin: 10px;
-`;
-
-export const SelectedCardImage = styled.img`
-  width: 60%;
-  height: 310px;
-  margin-left: 20px;
-  object-fit: cover;
-  border-radius: 12px;
-`;
-
-export const CardTitleCont = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 40%;
-  height: 100%;
-`;
-
-export const Price = styled.p`
-  font-weight: 400;
-  font-size: 20px;
-  line-height: 1.5;
-  margin-bottom: 20px;
-`;
-
-export const CountCont = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100px;
-  height: 50px;
-  border-radius: 15px;
-  background-color: var(--gray-color);
-  border: 1px solid var(--black-color);
-`;
-
-export const CountNumber = styled.p`
-  margin-left: 25px;
-  font-weight: 600;
-  font-size: 24px;
-  line-height: 1.5;
-`;
-
-export const ButtonsCont = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  border-radius: 15px;
-  color: var(--gray-color);
-`;
-
-export const CounterButton = styled.button`
-  border-radius: 10px;
-  background-color: var(--gray-color);
-`;
-
-export const XButton = styled.button`
-  position: absolute;
-  top: 0;
-  right: 0;
-  background-color: var(--white-color);
-  margin-top: 10px;
-  margin-right: 8px;
-`;
-
 export const AdditionalBlock = styled.div`
   height: 15vh;
   display: flex;
   align-items: center;
   justify-content: end;
-`;
-
-export const SelectedCardList = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  overflow-y: auto;
-  height: calc(100% - 87.6px);
 `;
 
 export const SelectedTotal = styled.p`
