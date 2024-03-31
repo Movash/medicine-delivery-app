b94eeb64bcffc1d44168dd78eede4f87c0090458
Movash <andrushhenko97@gmail.com>
2024-03-31 23:51:46 +0300

    sort_imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
 import { lazy } from 'react';
+import Layout from './components/Layout/Layout';
 const ShopsPage = lazy(() => import('pages/ShopsPage/ShopsPage'));
 const ShoppingCartPage = lazy(() =>
   import('pages/ShoppingCartPage/ShoppingCartPage')
