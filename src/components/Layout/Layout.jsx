0d06bc2443787387762e7a2b5d4c94d52ca8885b
Movash <andrushhenko97@gmail.com>
2024-04-01 16:53:35 +0300

    sort_imports

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
-import Header from 'components/Header/Header';
-import Loader from '../Loader/Loader';
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router';
+import Header from 'components/Header/Header';
+import Loader from '../Loader/Loader';
 
 const Layout = () => {
   return (
